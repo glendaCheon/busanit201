ca691542a3dcddc759f4135607cc4dd322f1114d
Glenda Cheon <cheon0915@naver.com>
2019-10-19 16:04:49 +0900

    서버소스 수정

--- server/users.js
+++ server/users.js
@@ -2,7 +2,12 @@ var express = requires('express');
 var router = express.router();
 
 router.post('/auth', function(req,res, next){
-res.json({id: req.body.id,
-        password:req.body.password})
+    const id = req.body.id;
+    const pwd =req.body.password;
+    res.json({result:(id=="test" && pwd=="1234") });
+// res.json({id: req.body.id,
+//         password:req.body.password})
 });
+
+
 
