623852731a464f57bbbb512f94ba010ea869f289
Glenda Cheon <cheon0915@naver.com>
2019-10-19 16:41:25 +0900

    되도록 커밋해놓음

--- server/routes/users.js
+++ server/routes/users.js
@@ -3,7 +3,16 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  // res.send('respond with a resource');
+  res.json({id: "test",
+        password:"id"})
+});
+
+router.post('/auth', function(req,res, next){
+  const id = req.body.id;
+  const pwd = req.body.password;
+  res.json({result:(id=="test" && pwd=="1234") });
+
 });
 
 module.exports = router;
